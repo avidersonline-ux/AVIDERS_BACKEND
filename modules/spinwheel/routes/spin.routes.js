c8d21ff06afa35f847748c3e0ba2bdaea769efc9
avidersonline-ux <avidersonline@gmail.com>
2025-11-26 22:51:18 +0530

    Update spin.routes.js

--- modules/spinwheel/routes/spin.routes.js
+++ modules/spinwheel/routes/spin.routes.js
@@ -1,15 +1,8 @@
 const router = require("express").Router();
-const auth = require("../../../middleware/firebaseAuth");
+const controller = require("../controllers/spin.controller");
 
-const {
-  spinNow,
-  spinStatus,
-  addBonusSpin
-} = require("../controllers/spin.controller");
-
-// All spin wheel actions require Firebase login
-router.get("/status", auth, spinStatus);
-router.post("/spin", auth, spinNow);
-router.post("/ad-spin", auth, addBonusSpin);
+router.get("/status", controller.spinStatus);
+router.post("/spin", controller.spinNow);
 
 module.exports = router;
+
