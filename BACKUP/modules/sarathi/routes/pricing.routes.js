0571a5cf3e7411e278be2733081f8bb3ba0e43bc
avidersonline-ux <avidersonline@gmail.com>
2025-11-26 21:29:50 +0530

    Delete BACKUP/modules/sarathi directory

--- BACKUP/modules/sarathi/routes/pricing.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getEstimate } = require("../controllers/pricing.controller");
-
-router.post("/estimate", getEstimate);
-
-module.exports = router;
--- BACKUP/modules/sarathi/routes/trips.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { createTrip, getTrip } = require("../controllers/trips.controller");
-
-router.post("/create", createTrip);
-router.get("/:id", getTrip);
-
-module.exports = router;
