0acca470c452b6e9eaeb8657c09e178093e6be28
avidersonline-ux <avidersonline@gmail.com>
2025-11-27 20:55:55 +0530

    Update SpinHistory.js

--- modules/spinwheel/models/SpinHistory.js
+++ modules/spinwheel/models/SpinHistory.js
@@ -1,9 +1,12 @@
+// modules/spinwheel-service/models/SpinHistory.js
+
 const mongoose = require("mongoose");
 
 const SpinHistorySchema = new mongoose.Schema({
   uid: { type: String, required: true },
-  reward: { type: Number, required: true },
   date: { type: Date, default: Date.now },
-}, { timestamps: true });
+  reward: Object,
+  amount: Number,
+});
 
 module.exports = mongoose.model("SpinHistory", SpinHistorySchema);
