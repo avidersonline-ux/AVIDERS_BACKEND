8837918076897760ba02b9f0f7638e400f35b928
avidersonline-ux <avidersonline@gmail.com>
2025-11-27 17:30:23 +0530

    Update mongo.js

--- config/mongo.js
+++ config/mongo.js
@@ -2,11 +2,18 @@ const mongoose = require("mongoose");
 
 const connectMongo = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    if (!process.env.MONGO_URI) {
+      console.error("❌ MONGO_URI missing in environment variables!");
+      process.exit(1);
+    }
 
-    console.log("🟢 MongoDB Connected (SpinWheel)");
+    await mongoose.connect(process.env.MONGO_URI, {
+      autoIndex: true,
+    });
+
+    console.log("✅ MongoDB Connected (Spinwheel)");
   } catch (err) {
-    console.error("🔴 MongoDB Connection Error:", err.message);
+    console.error("❌ MongoDB Connection Error:", err.message);
     process.exit(1);
   }
 };
