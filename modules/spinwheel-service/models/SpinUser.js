63ab98fd83565c4c7611313a689a21928f092491
avidersonline-ux <avidersonline@gmail.com>
2025-11-28 16:02:57 +0530

    Update SpinUser.js

--- modules/spinwheel-service/models/SpinUser.js
+++ modules/spinwheel-service/models/SpinUser.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const SpinUser = new mongoose.Schema(
   {
     userId: { type: String, required: true, unique: true },
+
     lastSpinDate: Date,
     bonusSpins: { type: Number, default: 0 },
     walletCoins: { type: Number, default: 0 }
