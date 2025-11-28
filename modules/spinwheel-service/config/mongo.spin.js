c1c4da3cf65cae9c536516ccf477bcbabce04670
avidersonline-ux <avidersonline@gmail.com>
2025-11-28 22:22:56 +0530

    Create mongo.spin.js

---
+++ modules/spinwheel-service/config/mongo.spin.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+module.exports = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI_SPIN, {});
+    console.log("✅ Spin DB Connected");
+  } catch (err) {
+    console.error("❌ Spin DB Error:", err.message);
+    process.exit(1);
+  }
+};
